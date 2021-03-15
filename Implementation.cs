89092f811c43e0f05adf4c244ae58b3d9ba3c304
ds5678 <jrpritts7@gmail.com>
2021-03-15 13:33:08 -0400

    Add project files.

---
+++ Implementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using Harmony;
+namespace LimitlessContainerSpace
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+        }
+    }
+
+    [HarmonyPatch(typeof(Container), "Awake")]
+    internal class Container_Awake
+    {
+        private static void Postfix(Container __instance)
+        {
+            __instance.m_CapacityKG = 10000;
+        }
+    }
+}
